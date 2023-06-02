0a874ae945d8dc76b5c21d313e633e90f9137623
TakashiAsanuma <rmacchoj7@gmail.com>
2023-06-02 10:55:39 +0900

    Learning Variables, Types

---
+++ numeric_perations/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+    println!("The sum is: {}", sum);
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("The difference is: {}", sum);
+
+    // multiplication
+    let product = 4 * 30;
+    println!("The product is: {}", sum);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+    println!("The quotient is: {}", sum);
+    println!("The truncated is: {}", sum);
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("The remainder is: {}", sum);
+}
---
+++ the_character_type/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+    println!("The c is: {}", c);
+    println!("The z is: {}", z);
+    println!("The heart_eyed_cat is: {}", heart_eyed_cat);
+}
