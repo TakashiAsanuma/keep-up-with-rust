8664a48092687de6f7ab4d7fefcf8102332b815d
TakashiAsanuma <rmacchoj7@gmail.com>
2023-06-02 10:17:46 +0900

    Start Common Programming Concepts

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);     // xの値は{}です
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const MAX_POINTS: u32 = 100_000;
+    println!("The const of x is: {}", MAX_POINTS);
+}
