b2e5e5fbe6c6fd60a8184b49c1db2c1bf45e917c
TakashiAsanuma <rmacchoj7@gmail.com>
2023-06-19 10:02:21 +0900

    Finished learning 5.Using Structs to Structure Related Data

--- using_structs_to_structure_related_data/defining_and_instantiating_structs/src/main.rs
+++ using_structs_to_structure_related_data/defining_and_instantiating_structs/src/main.rs
@@ -1,15 +1,11 @@
-struct User {
-    active: bool,
-    username: &str,
-    email: &str,
-    sign_in_count: u64,
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
 fn main() {
-    let user1 = User {
-        active: true,
-        username: "someusername123",
-        email: "someone@example.com",
-        sign_in_count: 1,
-    };
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    // rect1は{}です
+    println!("rect1 is {}", rect1);
 }
