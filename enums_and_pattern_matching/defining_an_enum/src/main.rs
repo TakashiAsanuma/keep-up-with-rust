503000d3a70dc555a7718930967729ed1df76267
TakashiAsanuma <rmacchoj7@gmail.com>
2023-06-19 15:40:15 +0900

    Start learning 6.Enums and Pattern Matching

---
+++ enums_and_pattern_matching/defining_an_enum/src/main.rs
@@ -0,0 +1,20 @@
+
+#![allow(unused)]
+fn main() {
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+        // メソッド本体はここに定義される
+    }
+}
+
+let m = Message::Write(String::from("hello"));
+m.call();
+}
