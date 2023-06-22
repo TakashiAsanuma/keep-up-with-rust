21410338b5c08be7e69cbfd985da0ad840569f4d
TakashiAsanuma <rmacchoj7@gmail.com>
2023-06-22 09:17:40 +0900

    Keep learning 7.Managing Growing Projects with Packages, Crates, and Modules

--- managing_growing_projects_with_packages_crates_and_odules/restaurant/src/lib.rs
+++ managing_growing_projects_with_packages_crates_and_odules/restaurant/src/lib.rs
@@ -1,17 +1,17 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
+}
 
-    mod serving {
-        fn take_order() {}
+pub fn eat_at_restaurant() {
+    // Absolute path
+    // 絶対パス
+    crate::front_of_house::hosting::add_to_waitlist();
 
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
+    // Relative path
+    // 相対パス
+    front_of_house::hosting::add_to_waitlist();
 }
 
 fn main() {}
