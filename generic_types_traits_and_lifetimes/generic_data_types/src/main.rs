e52c51a8ad784f31c5a4e25c3c53faa09f829cf3
TakashiAsanuma <rmacchoj7@gmail.com>
2023-06-27 10:00:44 +0900

    Keep learning 10. Generic Types, Traits, and Lifetimes

--- generic_types_traits_and_lifetimes/generic_data_types/src/main.rs
+++ generic_types_traits_and_lifetimes/generic_data_types/src/main.rs
@@ -1,29 +1,7 @@
-struct Point<T, U> {
-    x: T,
-    y: U,
-}
-
-impl<T, U> Point<T, U> {
-    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
-        Point {
-            x: self.x,
-            y: other.y,
-        }
-    }
-}
-
-fn main() {
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c'};
-
-    let p3 = p1.mixup(p2);
-
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
-}
-fn largest<T>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
-    for &item in list.iter() {
+    for &item in list {
         if item > largest {
             largest = item;
         }
