ee29c1b16cdb8df297550c625585b6904d7b2798
TakashiAsanuma <rmacchoj7@gmail.com>
2023-06-28 08:34:22 +0900

    Keep learning 11. Writing Automated Tests

---
+++ writing_automated_tests/greeting/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn greeting(name: &str) -> String {
+    String::from("Hello!")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            //挨拶(greeting)は名前を含んでいません。その値は`{}`でした
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+}
---
+++ writing_automated_tests/guess/src/lib.rs
@@ -0,0 +1,34 @@
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                //予想値は100以下でなければなりませんが、{}でした。
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                //予想値は1以上でなければなりませんが、{}でした。
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
